5d6955d1b8b5277e76e415757c473964120a7fac
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2021-11-25 19:31:45 +0100

    creating of environment variables

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React on {process.env.REACT_APP_BACKEND_WEB_SERVICE}
         </a>
       </header>
     </div>
